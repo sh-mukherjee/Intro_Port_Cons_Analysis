dfec2316f19030f7ba2e2af540ede29f9749af5d
sh-mukherjee <71715720+sh-mukherjee@users.noreply.github.com>
2024-06-05 11:33:34 +0800

    Updated with more functions

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -446,3 +446,59 @@ def summary_stats(r, risk_free_rate=0.03):
         "Sharpe Ratio": ann_sr,
         "Max Drawdown": dd
     })
+
+# def gbm(n_years=10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0):
+#     """
+#     Evolution of a Stock Price using a Geometric Brownian Motion Model
+#     """
+#     dt = 1/steps_per_year # delta in time, the change in time in each step, in this case it is one month
+#     n_steps = int(n_years*steps_per_year)
+#     rets_plus_1 = np.random.normal(loc=1+mu*dt, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios)) #loc is the mean, scale is the std dev
+#     rets_plus_1[0] = 1 #set the first row to 1
+#     # to prices
+#     prices = s_0*pd.DataFrame(rets_plus_1).cumprod()
+#     return prices
+
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val
+
+def discount(t, r):
+    """
+    Compute the price of a pure discount bond that pays a dollar at time t (in years), given annual interest rate r (assuming yield curve is flat, so r is constant)
+    """
+    return (1+r)**(-t)
+
+def pv(l, r):
+    """
+    Compute the present value of a sequence of liabilities l, given annual rate of interest r
+    l is indexed by the time, and the values are the amounts of each liability
+    Returns the present value of the sequence
+    """
+    dates = l.index
+    discounts = discount(dates, r)
+    return (discounts*l).sum()
+
+def funding_ratio(assets, liabilities, r):
+    """
+    Calculates ratio of assets / pv of liabilities calc at int rate r
+    """
+    return assets/pv(liabilities, r)
